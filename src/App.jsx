7d6714ddfcd82e3da738f2a308413cae0d9e19ac
Jerinislam05 <jerinislam05@gmail.com>
2024-05-11 09:25:55 +0100

    setup tailwind css with create react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+
+function App() {
+
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <h1 className='text-2xl font-bold text-center'>**Vite + React**</h1>
+    </>
+  )
+}
+
+export default App
