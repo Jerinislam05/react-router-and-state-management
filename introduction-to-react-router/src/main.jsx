65486e744138c4344ce51d22d6857b73bbba7cf7
Jerinislam05 <jerinislam05@gmail.com>
2024-05-15 19:42:45 +0100

    explore dynamic route, param and dynamic data

--- introduction-to-react-router/src/main.jsx
+++ introduction-to-react-router/src/main.jsx
@@ -9,6 +9,10 @@ import {
 } from "react-router-dom";
 import Home from './components/Home/Home';
 import Users from './components/Users/Users.jsx';
+import UserDetails from './components/Users/User/UserDetails/UserDetails.jsx';
+import Posts from './components/Posts/Posts.jsx';
+import Post from './components/Posts/Post/Post.jsx';
+import PostDetails from './components/PostDrtails/PostDetails.jsx';
 
 const router = createBrowserRouter([
   {
@@ -25,7 +29,23 @@ const router = createBrowserRouter([
       },
       {
         path: '/users',
+        loader: () => fetch('https://jsonplaceholder.typicode.com/users'),
         element: <Users></Users>
+      },
+      {
+        path: '/user/:userId',
+        loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+        element: <UserDetails></UserDetails>
+      },
+      {
+        path: '/posts',
+        loader: () => fetch('https://jsonplaceholder.typicode.com/posts'),
+        element: <Posts></Posts>
+      },
+      {
+        path: '/post/:postId',
+        loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
+        element: <PostDetails></PostDetails>
       }
     ]
   },
